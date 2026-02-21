c330e65a989a45a9579ca15091633a9c31932937
Raad Altaie <raad.altaie@icloud.com>
2026-02-21 02:25:57 -0600

    Update health-check.sh

--- scripts/health-check.sh
+++ scripts/health-check.sh
@@ -67,8 +67,8 @@ done
 if [[ $commit == true ]]
 then
   echo "committing logs"
-  git config --global user.name 'github-actions[bot]'
-  git config --global user.email 'github-actions[bot]@users.noreply.github.com'
+  git config --global user.name 'Raad [bot]'
+  git config --global user.email 'raad.altaie@icloud.com'
   git add -A --force public/status/
   git commit -am '[Automated] Update Health Check Logs'
   git push
